f3e5ff358db891d68666788d6c92a29a7c800411
Muhammad Mavia <Muhammadmavia172@gmail.com>
2021-09-30 15:19:38 +0500

    Updated

---
+++ app.js
@@ -0,0 +1,35 @@
+var num = 5.4645646;
+console.log(num.toFixed(20));
+console.log(String(num));
+console.log(num.toString());
+console.log('' + num);
+console.log(num + 5);
+console.log(+num + 5);
+console.log(num - 5);
+console.log(num * 5);
+console.log(num);
+console.log(parseInt(num));
+console.log(parseFloat(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+console.log(Number(num));
+console.log(+num);
+
+
+/*var str = 'Hello World';
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.split());
+console.log(str.split(''));
+console.log(str.split(' '));
+console.log(str.slice(2, 4));
+console.log(str.slice(2));
+console.log(str.replace(' ', '*'));
+console.log(str.substring(2, 4));
+console.log(str.charAt(6));
+console.log(str[6]);
+console.log(str.indexOf('l'));
+console.log(str.lastIndexOf('l'));
+console.log('A'.charCodeAt());
+console.log('a'.charCodeAt());*/
